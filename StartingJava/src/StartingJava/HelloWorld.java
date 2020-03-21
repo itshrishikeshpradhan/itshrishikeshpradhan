a4fed0b458a96cb4754a5c7e183a14434a3ecec7
hrishikesh <hrishikesh3228@gmail.com>
2020-03-21 18:50:04 +0530

    starting_java

---
+++ StartingJava/src/StartingJava/HelloWorld.java
@@ -0,0 +1,9 @@
+package StartingJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+System.out.println("hello I have started java");
+	}
+
+}
---
+++ StartingJava/src/module-info.java
@@ -0,0 +1,2 @@
+module StartingJava {
+}
